9c806f325b3aa015ff91e58d826a5d9af44fd25d
João Victor da Silva <jv147002@gmail.com>
2023-02-13 16:52:39 -0300

    terminando

--- exercícios/app.js
+++ exercícios/app.js
@@ -5,6 +5,8 @@
  * Versão: 1.0
  *********************************************************************************/
 
+var calcularMedia = require('./modulo/media.js')
+
 var readline = require('readline')
 
 var entradaDados = readline.createInterface({
@@ -17,7 +19,7 @@ entradaDados.question('Digite o nome de seu aluno: \n', function(nomeAluno){
 
     entradaDados.question('Qual sexo: [Masculino | Feminino] \n', function(tipoDeSexo){
         let tipoSexoAluno = tipoDeSexo
-        let operacao = tipoDeSexo.toUpperCase()
+        // let operacao = tipoDeSexo.toUpperCase()
 
        entradaDados.question('Digite o nome do Professor: \n', function(nomeProfessor){
 
@@ -25,7 +27,7 @@ entradaDados.question('Digite o nome de seu aluno: \n', function(nomeAluno){
 
             entradaDados.question('Digite o seu sexo: [Masculino | Feminino] \n', function(sexoProfessor){
                 let tipoSexoProfessor = tipoDeSexo
-                let operacao = sexoProfessor.toUpperCase()
+                // let operacao = sexoProfessor.toUpperCase()
 
                 entradaDados.question('Nome do curso : \n', function(cursoAluno){
                     let curso = cursoAluno
@@ -33,15 +35,43 @@ entradaDados.question('Digite o nome de seu aluno: \n', function(nomeAluno){
                    entradaDados.question('Nome da disciplina: \n', function(nomeDisciplina){
                        let disciplina = nomeDisciplina
 
-                       entradaDados.question('Nota1 :')
-                   })
+                       entradaDados.question('Nota1 : \n', function(nota1){
+                            let valor1 = nota1.replace(',' , '.')
+
+                            entradaDados.question('Nota2 : \n', function(nota2){
+                                let valor2 = nota2.replace(',' , '.')
+
+                                entradaDados.question('Nota3 : \n', function(nota3){
+                                    let valor3 = nota3.replace(',' , '.')
+
+                                    entradaDados.question('Nota4 : \n', function(nota4){
+                                        let valor4 = nota4.replace(',' , '.')
+                                        let media 
+
+                                        mediaAluno = calcularMedia.mediaAluno(valor1, valor2, valor3, valor4)
+
+                                        if ( mediaAluno >= 50 && mediaAluno <= 69){
+                                            console.log('O aluno precia fazer um exame :')
+                                            entradaDados.question('Digite uma nota do exame : \n', function(notaExame){
+                                               
+                                                
+                                                
+                                            })
+                                        }
+                                        
+                                        
+
+                                        
+
+                                        
+                                    })
+                                })
+                            })
+                        })
+                    })
                 })
-                
-            })
-        })
-
-    })
-
-      
+            })  
+        })  
+    })   
 })
 
--- exercícios/modulo/media.js
+++ exercícios/modulo/media.js
@@ -7,42 +7,97 @@
 
 
 
-const mediaAluno = function (nota1, nota2, nota3, nota4, media) {
+const mediaAluno = function ( nota1, nota2, nota3, nota4, ) {
 
+    let n1 = Number(nota1)
+    let n2 = Number(nota2)
+    let n3 = Number(nota3)
+    let n4 = Number(nota4)
 
-    media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) / 4
+    media = (parseFloat( n1) + parseFloat(n2) + parseFloat(n3) + parseFloat(n4)) / 4
 
-    if (nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '') {
+    if ( n1 == '' || n2 == '' || n3 == '' || n4 == '') {
         console.log('Erro vc não digitou um valor')
-    } else if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
+    } else if (isNaN( n1) || isNaN(n2) || isNaN(n3) || isNaN(n4)) {
         console.log('Erro: não pode utilizar texto')
-    } else if (nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100) {
+    } else if ( n1 > 100 || n2 > 100 || n3 > 100 || n4 > 100) {
         console.log('Digite um valor menor ou igual a 100')
-    } else if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0) {
+    } else if ( n1 < 0 || n2 < 0 || n3 < 0 || n4 < 0) {
         console.log('Digite um valor maior que 0 ou igual a 0')
     } else if (media >= 70) {
-        console.log('aprovado')
-    }else if (media >= 50 && media <= 69) {
+        // console.log('aprovado')
+        return media
+    } else if (media >= 50 && media <= 69) {
+        // console.log('exame');
         exame()
-        console.log('exame');
-    }else if (media <= 50) {
-        console.log('reprovadwwwwwwwwwwwo')
+        return media
+    } else if (media <= 50) {
+        // console.log('reprovado')
+        return media
     }
 }
-const  exame = function (mediaAluno, notaExame){
-    let exame = (mediaAluno + notaExame) /2
 
-    if(exame >= 60){
-        console.log('aprovado')
-    }else if( exame <= 59) {
-        console.log('reprovado')
+const exame = function (mediaAluno, notaExame) {
+
+    let exame = (Number(mediaAluno) + Number(notaExame)) / 2
+
+    if (exame == '') {
+        console.log('ERRO: entrada está vazia');
+    } else if (isNaN(exame)) {
+        console.log('ERRO: Digite número válido e não letras');
+    } else if (exame > 100) {
+        console.log('ERRO: A nota não pode ser maior que 100');
+    } else if (exame < 0) {
+        console.log('ERRO: A nota não pode ser menor que 0');
+    } else if (exame >= 69) {
+        // console.log('aprovado')
+        return exame
+    } else if (exame <= 50) {
+        // console.log('reprovado')
+        return exame
     }
 }
+
+
+const sexoAluno = function (sexoAl) {
+    let generoAl = sexoAl
+
+    switch (generoAl) {
+
+        case 'MASCULINO':
+            generoAl = 'O aluno'
+            break
+
+        case 'FEMININO':
+            generoAl = 'A aluna'
+            break
+
+    }
+}
+
+const sexoProfessor = function (sexoProf) {
+    let generoProf = sexoProf
+
+    switch (generoProf) {
+
+        case 'MASCULINO':
+            generoProf = 'O Professor'
+            break
+
+        case 'FEMININO':
+            generoProf = 'A Professora'
+            break
+
+    }
+}
+
+
 
 mediaAluno(50, 50, 50, 60)
 exame(53, 80)
 
 module.exports = {
-    mediaAluno
+    mediaAluno,
+    exame,
 }
 
